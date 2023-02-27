8155912df06eb6fdf7f3173ededa9d2161eec764
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-02-27 23:58:56 +0530

    Update _document.js
    added OG tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="AI MAHESHWARI" key="title"/>
+        <meta property="og:description" content="Get motivated and reach your goals with AI" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://i.ibb.co/T1gRmBR/Final-OG-AI.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
