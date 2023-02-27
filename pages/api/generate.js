3c9c65e66a83caa607a191273ee45a3ac7643b8e
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-02-27 23:28:32 +0530

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,46 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Motivate me. ";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 1250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  const secondPrompt = 
+  `
+Take motivation given below and convert it in what Sandeep Maheshwari would say 
+
+  Motivation: ${basePromptOutput.text}
+
+  Sandeep Maheshwari:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
---
+++ pages/index.js
@@ -0,0 +1,91 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import logo from './chatbot logo.png'
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+const [isElement,setIsElement] = useState(true)
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  function sendMessage(){
+    const p = document.createElement('p')
+
+    if(userInput.length > 0){
+    p.innerText = userInput;
+    p.classList.add('chat')
+    document.getElementById('send').appendChild(p)
+    setIsElement(!isElement)
+    }
+    if(isElement === false){
+p.classList.add('visible')
+      setIsElement(true)
+    }
+    setUserInput('')
+    callGenerateEndpoint()
+  }
+return (
+    <div className="root">
+      <Head>
+        <title>AI Maheshwari 🤖</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>AI  Maheshwari 🤖</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Let AI motivates you in the style of Sir Sandeep Maheshwari</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <input 
+          type='text'
+          placeholder="Why do you need motivation?..." 
+          className="prompt-box" 
+          value={userInput}
+  onChange={onUserChangedText}
+          />
+        <button className="generateButton" onClick={callGenerateEndpoint}>    {isGenerating ? <span className="loader"></span> : 'Get Motivation'}
+</button>
+        </div>
+{apiOutput && 
+        <div className="output" >
+          <div className="outputNav">
+          <img className='img3' src={'https://i.ibb.co/XC8bSZ8/chatbot-logo.png'} alt="" />
+<h3>AI Maheshwari</h3>
+          </div>
+          <div className="outputContent">
+            <div className="images">
+            <img className = 'img1' src="https://e7.pngegg.com/pngimages/149/889/png-clipart-quotation-mark-symbol-computer-icons-font-awesome-kartikeya-text-logo.png" alt="" />
+          <img className='img2' src="https://e7.pngegg.com/pngimages/840/476/png-clipart-quotation-mark-font-awesome-computer-icons-quotation-text-logo.png" alt="" />
+            </div>
+            <p>{apiOutput}
+</p>
+          </div>
+
+        </div>}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
