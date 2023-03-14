091b49e4a9a0439e428b45ac4aed4cbc70cf274d
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-03-14 23:42:13 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import './styles.css';
-import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
   return (
-  <>
-  <Component {...pageProps} />
-  <Analytics />
-  </>)
+  <Component {...pageProps} />)
 }
 export default App;
